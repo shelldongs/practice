5cab329f59a07df860b97dd566e99ada46e0d0a5
xiaodong <634750125@qq.com>
2018-03-15 03:04:49 -0400

    append test

--- kylincache.py
+++ kylincache.py
@@ -1,4 +1,7 @@
+# coding:utf-8
+
 import time
+
 
 def dict_sorted(d):
     return sorted(d.items(), key=lambda item:item[1])
@@ -50,58 +53,101 @@ class KylinCache(object):
                 del self.data_expire[key]
                 self.count -= 1
 
-        data_expire_list = dict_sorted(self.data_expire)
-        for d in data_expire_list:
-            key = d[0]
-            expire = d[1]
-            if expire < time.time():
-                del self.data[key]
-                del self.data_expire[key]
-                self.count -= 1
-            else:
-                return
+            for i in range(clean_len, self.count):
+                d = data_expire_list[i]
+                key = d[0]
+                expire = d[1]
+                if expire < time.time():
+                    del self.data[key]
+                    del self.data_expire[key]
+                    self.count -= 1
+                else:
+                    return
 
+  #  def _clean_data(self):
+  #      if self.count > self.max_size - 1:
+  #          data_expire_list = dict_sorted(self.data_expire)
+  #          
+  #          clean_len = int(self.max_size * (1 - self.CLEAN_DATA_PERCENT))
+  #          
+  #          for i in range(clean_len):
+  #              key = data_expire_list[i][0]
+  #              del self.data[key]
+  #              del self.data_expire[key]
+  #              self.count -= 1
+
+  #      data_expire_list = dict_sorted(self.data_expire)
+  #      for d in data_expire_list:
+  #          key = d[0]
+  #          expire = d[1]
+  #          if expire < time.time():
+  #              del self.data[key]
+  #              del self.data_expire[key]
+  #              self.count -= 1
+  #          else:
+  #              return
+
+    def __call__(self, *args, **kwrags):
+        if len(args) >= 1:
+            timeout = args[0]
+        else:
+            timeout = self.timeout
+
+        def wrapper(func):
+            def _wrapper(*args, **kwargs):
+                key = args[0]
+                value = self.get(key)
+                if value:
+                    print 'get cache'
+                    return value
+                ret = func(*args, **kwargs)
+                print 'hit db'
+                self.set(key, ret, timeout=timeout)
+                return ret
+
+            return _wrapper
+
+        return wrapper    
 
     def print_data(self):
         print self.data
         print self.data_expire
-        print '-'*80
+
+if __name__ == "__main__":
+    cache = KylinCache(max_size=10)
+
+    @cache(5, 2, _=3)
+    def test(key):
+        return key
+
+    test(1)
+    test(2)
+    test(1)
+
+    r = KylinCache(max_size=10)
+    r.set(1,'a')
+    r.set(2,'a')
+    r.set(3,'a')
+    r.set(4,'a')
+    r.set(5,'a')
+    r.set(6,'a')
+    r.set(7,'a')
+    r.set(8,'a')
+    r.set(9,'a')
+    r.print_data()
+
+    r.set(10,'a')
+    r.set(11,'a')
+    r.print_data()
 
 
-r = KylinCache(max_size=10)
-r.set(1,'a')
-r.set(2,'a')
-r.set(3,'a')
-r.set(4,'a')
-r.set(5,'a')
-r.set(6,'a')
-r.set(7,'a')
-r.set(8,'a')
-r.set(9,'a')
-r.print_data()
-
-r.set(10,'a')
-r.set(11,'a')
-r.print_data()
-
-time.sleep(5)
-print time.time()
-r.print_data()
-
-time.sleep(11)
-print time.time()
-r.set(12, 'a')
-r.print_data()
-
-
-
-
-
-
-
-
-
-
-
-
+    time.sleep(12)
+    print time.time()
+    r.set(12, 'a')
+    r.print_data()
+    r.set(13, 'a')
+    r.print_data()
+    print time.time()
+    r.set(14, 'a')
+    r.print_data()
 
